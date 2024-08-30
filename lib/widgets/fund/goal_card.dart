ace2a8601015c9d94b4cd59fdb80e73d3cec2103
seojiheun <wlgmsaudqls@naver.com>
2024-08-31 03:14:21 +0900

    fix: Fund 입출금 시 잔액 확인 modal

--- lib/widgets/fund/goal_card.dart
+++ lib/widgets/fund/goal_card.dart
@@ -116,7 +116,7 @@ class _GoalCardState extends State<GoalCard> {
                             widget.reloadData();
                           });
                         },
-                        child: Text('충전하기'),
+                        child: Text('충하기'),
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.blue,
                           foregroundColor: Colors.white,
